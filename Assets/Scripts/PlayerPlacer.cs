6279bb5f29039ddb3dee04c3b4d3a7c72d2bf4d1
Timmy-Codes <63141747+Timmy-Codes@users.noreply.github.com>
2024-09-27 20:55:37 +1000

    Update PlayerPlacer.cs

--- Assets/Scripts/PlayerPlacer.cs
+++ Assets/Scripts/PlayerPlacer.cs
@@ -4,13 +4,9 @@ using UnityEngine.SceneManagement;
 public class PlayerPlacer : MonoBehaviour
 {
     private Vector3 startPos;
-<<<<<<< HEAD
 
     private bool hasPlacedPlayer = false;
 
-=======
-    //private bool hasPlacedPlayer = false; this is never used and so I commented it out..
->>>>>>> parent of 1acd4f1 (Merge pull request #18 from Fulminin/Will's-Branch)
     private Quaternion startRot;
 
     private void OnEnable()
@@ -26,12 +22,8 @@ public class PlayerPlacer : MonoBehaviour
 
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-<<<<<<< HEAD
-
         hasPlacedPlayer = false;
-=======
-        // hasPlacedPlayer = false;
->>>>>>> parent of 1acd4f1 (Merge pull request #18 from Fulminin/Will's-Branch)
+
         // placed a bool because it was sometimes placing teh player twice
 
         GameObject respawnObject = GameObject.FindGameObjectWithTag("Respawn");
@@ -57,13 +49,9 @@ public class PlayerPlacer : MonoBehaviour
 
                     Debug.Log("Actual Player Position after placement: " + player.transform.position);
 
-<<<<<<< HEAD
 
                     hasPlacedPlayer = true;
 
-=======
-                    // hasPlacedPlayer = true;
->>>>>>> parent of 1acd4f1 (Merge pull request #18 from Fulminin/Will's-Branch)
                     // this if gets the player and places them in the spawn point. tyhere is a bit of debugging as well, to make sure it is working
                     // had a bit of a problem with this as sometimes it works sometimes it doesnt. If it doesnt work it just leaves the player in the same position as scene 1
                     // to counter this i moved scene 1 to the dsame position as scene 2, so if it doenst work it should hopefully drop the player in the same room in scene 2
