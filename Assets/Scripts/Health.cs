3ae5b4c182df210264ddc7c393ca374e82d18222
Corimo <MattCarlile590@gmail.com>
2024-10-09 18:42:20 +1100

    heal after 10 seconds

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -6,10 +6,12 @@ using UnityEngine.UI;
 public class Health : MonoBehaviour
 {
     public int maxHealth = 100;
+    public float waitTime = 10.0f;
 
     private int healthNumber;
     private Text healthDisplay;
     private PlayerPlacer playerPlacer;
+    private float healTimer = 0.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -37,6 +39,15 @@ public class Health : MonoBehaviour
 
             healthNumber = maxHealth;
 
+        }
+        if (healthNumber < maxHealth)
+        {
+            healTimer += Time.deltaTime;
+            if (healTimer > waitTime)
+            {
+                healthNumber = maxHealth;
+                healTimer = 0.0f;
+            }
         }
     }
 
